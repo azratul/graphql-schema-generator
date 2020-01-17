4948b3484a983b860523dae23efcacb1c53e344e
claudio.lepin <claudio.lepin@uchile.cl>
2020-01-17 18:42:09 -0300

    Fix: App's name

--- README.md
+++ README.md
@@ -13,9 +13,11 @@ go install github.com/azratul/graphql-schema-generator
 ## How to Use
 
 ```zsh
-schemagen -h
+graphql-schema-generator -h
 ```
 
-Example:
+### Example:
 
-schemagen -user="DB_USER" -password="DD_PASS" -dsn="(DESCRIPTION=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=DB_HOST)(PORT=DB_PORT))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=db.orcl.com)))" -entities=TABLE1,TABLE2,TABLE3
+```zsh
+graphql-schema-generator -user="DB_USER" -password="DD_PASS" -dsn="(DESCRIPTION=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=DB_HOST)(PORT=DB_PORT))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=db.orcl.com)))" -entities=TABLE1,TABLE2,TABLE3
+```
--- schemagen.go
+++ schemagen.go
@@ -16,10 +16,10 @@ var user *string
 var password *string
 
 func init(){
-    dsn = flag.String("dsn", "", "Data source name, ex: schemagen -dsn=\"(DESCRIPTION=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL={PROTOCOL})(HOST={HOST})(PORT={PORT}))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={SERVICE_NAME})))\"")
-    entities = flag.String("entities", "", "Array of entities(comma separated), ex: 'schemagen -entities=table1,table2,table3'")
-    user = flag.String("user", "", "Database user, ex: schemagen -user=\"DB_USER\"")
-    password = flag.String("password", "", "Database password, ex: schemagen -password=\"PASSWORD1234\"")
+    dsn = flag.String("dsn", "", "Data source name\nEx:\n\tgraphql-schema-generator -dsn=\"(DESCRIPTION=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL={PROTOCOL})(HOST={HOST})(PORT={PORT}))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={SERVICE_NAME})))\"")
+    entities = flag.String("entities", "", "Array of entities(comma separated)\nEx:\n\tgraphql-schema-generator -entities=table1,table2,table3")
+    user = flag.String("user", "", "Database user\nEx:\n\tgraphql-schema-generator -user=\"DB_USER\"")
+    password = flag.String("password", "", "Database password\nEx:\n\tgraphql-schema-generator -password=\"PASSWORD1234\"")
 
     flag.Parse()
 
