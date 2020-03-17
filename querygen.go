4f71ff7accb5220fcea426745da7d04d84993d34
Claudio Lepin <claudio.lepin@uchile.cl>
2020-03-17 18:35:15 -0300

    Add definition schema

--- querygen.go
+++ querygen.go
@@ -5,6 +5,7 @@ import (
     "log"
     "os"
     "strings"
+    "strconv"
     "database/sql"
 
     //Oracle
@@ -62,10 +63,11 @@ func makeSchemas(db *sql.DB, entities []string) string {
     var queryInsert  string
     var queryInsert2 string
     var queryUpdate  string
+    var definition   string
 
     if *motor == "godror" {
         // oracle
-        query = `SELECT COLUMN_NAME FROM ALL_TAB_COLUMNS WHERE UPPER(TABLE_NAME)=UPPER(:1) AND UPPER(OWNER)=UPPER(:2) ORDER BY COLUMN_ID`
+        query = `SELECT COLUMN_NAME, DATA_TYPE, DATA_SCALE FROM ALL_TAB_COLUMNS WHERE UPPER(TABLE_NAME)=UPPER(:1) AND UPPER(OWNER)=UPPER(:2) ORDER BY COLUMN_ID`
     } else {
         // postgres or mysql
         bind := [2]string{"?", "?"}
@@ -73,7 +75,7 @@ func makeSchemas(db *sql.DB, entities []string) string {
             bind = [2]string{"$1", "$2"}
         }
 
-        query = `SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=` + bind[0] + ` AND TABLE_SCHEMA=` + bind[1] + ``;
+        query = `SELECT COLUMN_NAME, DATA_TYPE, NUMERIC_SCALE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME=` + bind[0] + ` AND TABLE_SCHEMA=` + bind[1] + ``;
     }
 
     stm, err := db.Prepare(query)
@@ -94,21 +96,58 @@ func makeSchemas(db *sql.DB, entities []string) string {
 	queryInsert  = entity + ":INSERT INTO " + entity + "("
 	queryInsert2 = ""
 	queryUpdate  = entity + ":UPDATE " + entity + " SET "
+	definition   = entity + ":DEFINITION:"
         for rows.Next() {
             var column_name string
-            if err := rows.Scan(&column_name); err != nil {
+            var data_type string
+            var data_scale []byte
+            if err := rows.Scan(&column_name, &data_type, &data_scale); err != nil {
                 log.Fatalf("Scan error: %s",err)
+            }
+
+            data_type = strings.ToUpper(data_type)
+
+            if  data_type == "VARCHAR" ||
+                data_type == "VARCHAR2" ||
+                data_type == "CHAR" ||
+                data_type == "TEXT" {
+                data_type = "string"
+            } else if data_type == "DATE" ||
+                data_type == "DATETIME" {
+                data_type = "time"
+            } else if data_type == "BOOLEAN" {
+                data_type = "bool"
+            } else {
+                i, _ := strconv.Atoi(string(data_scale))
+
+                if i > 0 {
+                    data_type = "float"
+                } else {
+                    data_type = "int"
+                }
             }
 
 	    querySelect  += column_name + ","
 	    queryInsert2 += ":" + column_name + ","
 	    queryUpdate  += column_name + " = :" + strings.ToLower(column_name) + ","
+	    title := ""
+
+            if strings.ToUpper(column_name) == "ID" {
+		title = strings.ToUpper(column_name)
+            } else {
+	        title = strings.Replace(strings.ToLower(column_name),"_"," ",-1)
+	        title = strings.Title(title)
+	        title = strings.Replace(title," ","",-1)
+            }
+
+	    definition += title + "," + column_name + "," + data_type + ";"
         }
 	querySelect  = strings.TrimRight(querySelect, ",")
 	queryInsert2 = strings.TrimRight(queryInsert2, ",")
 	queryInsert += strings.Replace(queryInsert2, ":", "", -1)
 	queryUpdate  = strings.TrimRight(queryUpdate, ",")
-        data += querySelect + " FROM " + entity + " WHERE 1=1\n" + queryInsert + ") VALUES (" + strings.ToLower(queryInsert2) + ")\n" + queryUpdate + " WHERE 1=1\n\n"
+	definition   = strings.TrimRight(definition, ";")
+        data += querySelect + " FROM " + entity + " WHERE 1=1\n" + queryInsert + ") VALUES (" + strings.ToLower(queryInsert2) + ")\n" + queryUpdate + " WHERE 1=1\n" + definition + "\n\n"
     }
 
     return data
